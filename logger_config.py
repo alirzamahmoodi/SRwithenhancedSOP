300976bdb632fe6274ae871fe490d39af4f529a5
alirzamahmoodi <alirezamahmoodi5111@gmail.com>
2025-03-03 17:52:39 +0330

    bundled app to directly add the output to database.

---
+++ logger_config.py
@@ -0,0 +1,11 @@
+import logging
+
+def setup_logging(log_file='app.log'):
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler(log_file),
+            logging.StreamHandler()
+        ]
+    )
